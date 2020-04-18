27a1fa1b4c57d6fe754802055b8f2f0bdc8de9dd
MohamedAshraf004 <lionelmido004@gmail.com>
2020-04-18 16:20:35 +0200

    Upload App

---
+++ SecureClient/AuthConfig.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Globalization;
+
+
+namespace SecureClient
+{
+    public class AuthConfig
+    {
+        public string Instance {get; set;} =
+      "https://login.microsoftonline.com/{0}";
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string Authority 
+        { 
+            get
+            {
+                return string.Format(CultureInfo.InvariantCulture,Instance,TenantId);
+            }
+        }
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceId { get; set; }
+
+        public static AuthConfig ReadJsonFromFile(string path)
+        {
+            IConfiguration Configuration;
+            var builder= new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(path);
+
+             Configuration=builder.Build();
+
+            return Configuration.Get<AuthConfig>();       
+        }
+    }
+}
